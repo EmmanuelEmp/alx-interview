03b08912e05507b6accf9bf410ac8121d3b7f900
EmmanuelEmp <emmyochogwu@gmail.com>
2023-08-21 01:01:13 +0100

    Rotate matrix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,47 +1,54 @@
 #!/usr/bin/python3
-"""Solves the N queens puzzle challenge"""
+"""Solving N Queens with Backtracing"""
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
 
-try:
-    N = int(sys.argv[1])
-    if N < 4:
+def nqueens(n, y, board):
+    """
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form
+    """
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
 
+    nqueens(n, 0, [])
 
-def resolve_matrix_for(queen, matrix, positions):
-    """
-    Resolves/removes reachable range of queen from the matrix then
-    recursively resolves for subsequent possible queen positions
-    for the next row.
-    """
-    positions.append(queen)
-
-    for cell in matrix[:]:
-        if any([cell == queen, cell[0] == queen[0], cell[1] == queen[1],
-                cell[0] - cell[1] == queen[0] - queen[1],
-                cell[0] + cell[1] == queen[0] + queen[1]]):
-            matrix.pop(matrix.index(cell))
-
-    # if end of the recursion
-    if len(matrix) == 0:
-        if len(positions) == N:
-            print(positions)
-        return
-
-    # else recursively check for possible queen positions
-    for possible_queen_pos in [cell for cell in matrix
-                               if cell[0] == matrix[0][0]]:
-        resolve_matrix_for(possible_queen_pos, matrix[:], positions[:])
-
-
-for i in range(N):
-    resolve_matrix_for([0, i], [[x, y] for x in range(N)
-                       for y in range(N)], [])
+if __name__ == '__main__':
+    main()
